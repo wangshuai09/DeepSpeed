14f6cf204cc7ff4163832ffbe30ac6a301687f3a
wangshuai09 <391746016@qq.com>
2023-12-05 11:47:59 +0800

    update

--- op_builder/npu/transformer_inference.py
+++ op_builder/npu/transformer_inference.py
@@ -30,6 +30,33 @@ def swap_two_rows(x):
     return x
 
 
+def ensure_divisibility(numerator, denominator):
+    """Ensure that numerator is divisible by the denominator."""
+    assert numerator % denominator == 0, "{} is not divisible by {}".format(
+        numerator, denominator
+    )
+
+
+def divide(numerator, denominator):
+    """Ensure that numerator is divisible by the denominator and return
+    the division value."""
+    ensure_divisibility(numerator, denominator)
+    return numerator // denominator
+
+
+def split_tensor_along_last_dim(tensor, num_partitions, contiguous_split_chunks=False):
+    # Get the size and dimension.
+    last_dim = tensor.dim() - 1
+    last_dim_size = divide(tensor.size()[last_dim], num_partitions)
+    # Split.
+    tensor_list = torch.split(tensor, last_dim_size, dim=last_dim)
+    # Note: torch.split does not create contiguous tensors by default.
+    if contiguous_split_chunks:
+        return tuple(chunk.contiguous() for chunk in tensor_list)
+
+    return tensor_list
+
+
 class InferenceContext:
     _workspace = None
 
@@ -45,7 +72,7 @@ class InferenceContext:
 
     workSpaceSize = 0
 
-    kv_cache = None
+    kv_caches = None
 
     @staticmethod
     def reset_tokens(initial_tokens=1):
@@ -71,15 +98,22 @@ class InferenceContext:
 
 class NPUInference():
 
-    @classmethod
-    def _qkv_gemm(cls, input, weight, q_scale, bias, gamma, beta, epsilon,
-                      add_bias, q_int8, transposed_mode):
-        inp_norm = torch.nn.functional.layer_norm(input, (input.shape[2],),
-                                                  gamma, beta, epsilon)
+    # def _qkv_gemm(cls, input, weight, q_scale, bias, gamma, beta, epsilon,
+    #                   add_bias, q_int8, transposed_mode):
+    #     inp_norm = torch.nn.functional.layer_norm(input, (input.shape[2],),
+    #                                               gamma, beta, epsilon)
+    #     tmp = torch.matmul(inp_norm, weight.t())
+    #     if add_bias:
+    #         tmp += bias
+        
+    #     output = [tmp, inp_norm]
+    #     return output
+    @staticmethod
+    def _qkv_gemm(inputs, weight, q_scale, bias, gamma, beta, eps, add_bias, q_int8, transpose):
+        inp_norm = torch.nn.functional.layer_norm(inputs, (inputs.shape[2],), gamma, beta, eps)
         tmp = torch.matmul(inp_norm, weight.t())
         if add_bias:
             tmp += bias
-        
         output = [tmp, inp_norm]
         return output
 
@@ -103,17 +137,14 @@ class NPUInference():
                       add_bias, q_int8, transposed_mode)
     
     @classmethod
-    def _bias_add_transform_0213(cls, output, k_cache, v_cache, vals, bias, 
+    def _bias_add_transform_0213(cls, vals, bias, 
                                  hidden_dim, seq_length, seq_offset, heads,
                                  num_kv, # num_kv > 0 ? num_kv : heads,
                                  rotary_dim,
                                  rotate_half,
                                  rotate_every_two,
                                  rope_theta):
-        # q,k,v
-        # q shape: [bsz, seq, heads, head_dim]
-        # k shape: [bsz, seq, num_kv, head_dim]
-        # v shape: [bsz, seq, num_kv * head_dim]
+        # vals: [bsz, seq_len, 3*heads*d]
         bsz, _, _ = vals.shape
         q = vals[..., :hidden_dim].reshape(bsz, seq_length, heads, -1)
         k = vals[..., hidden_dim: hidden_dim + num_kv * (hidden_dim // heads)].reshape(bsz, seq_length, num_kv, -1)
@@ -143,13 +174,14 @@ class NPUInference():
             k_pos = k_pos * cos + swap_two_rows(k_pos) * sin
             k = torch.cat([k_pos, k_pass], dim=-1)
     
-        # 结果，v 不变, shape: [bsz, heads, seq, head_dim]
+        # return 
+        # ouput: [bsz, seq_len, heads*d]
+        # k_cache: [bsz, seq_len, heads*d]
+        # v_cache: [bsz, seq_len, heads*d]
         output = q.reshape(bsz, seq_length, -1).contiguous()
         k_cache= k.reshape(bsz, seq_length, -1).contiguous()
         v_cache = v.contiguous()
-        # output = q.permute(0,2,1,3).contiguous()
-        # k_cache= k.permute(0,2,1,3).contiguous()
-        # v_cache = v.permute(0,2,1,3).contiguous()
+       
         print("result:", output.shape, k_cache.shape, v_cache.shape)
         return output, k_cache, v_cache
         
@@ -163,23 +195,22 @@ class NPUInference():
         hidden_dim = heads * k 
        
         is_promt = seq_len > 1
+
+        if not InferenceContext.kv_caches:
+            InferenceContext.kv_caches = [[None, None] for _ in range(num_layers)]
         if is_promt:
             InferenceContext.reset_tokens(seq_len)
-        
+            InferenceContext.kv_caches[layer_id] = [None, None]
+
         soft_len = InferenceContext.current_tokens()  
         workspace = InferenceContext.GetWorkSpace()  
         seq_offset = 0 if is_promt else soft_len - 1
         
         # 
         print("soft_len", soft_len, "layer_id", layer_id, "num_layers", num_layers)
+        print("alibi", alibi)
 
-        output = torch.empty((bsz, seq_len, heads * k), dtype=torch.float16, device="npu")
-        k_cache = torch.empty((bsz, seq_len, (num_kv if num_kv > 0 else heads) * k), dtype=torch.float16, device="npu")
-        v_cache = torch.empty((bsz, seq_len, (num_kv if num_kv > 0 else heads) * k), dtype=torch.float16, device="npu")
-        q, k, v = NPUInference._bias_add_transform_0213(output=output, 
-                                              k_cache=k_cache, 
-                                              v_cache=v_cache, 
-                                              vals=query_key_value, 
+        q, k, v = NPUInference._bias_add_transform_0213(vals=query_key_value, 
                                               bias=None, 
                                               hidden_dim=hidden_dim, 
                                               seq_length=seq_len, 
@@ -190,44 +221,61 @@ class NPUInference():
                                               rotate_half=rotate_half,
                                               rotate_every_two=rotate_every_two,
                                               rope_theta=rope_theta)
+
+        print("qkv shape after bias_add_transform_0213:", q.shape, k.shape, v.shape)
         torch.npu.synchronize()
-        try:
-            if not is_promt: # use_cache
-                if not InferenceContext.kv_cache:
-                    InferenceContext.kv_cache = [[None, None] for _ in range(num_layers)]
-                print(InferenceContext.kv_cache[layer_id])
-                k_cache, v_cache = InferenceContext.kv_cache[layer_id]
-                if k_cache:
-                    k = torch.cat((k_cache, k), 1)
-                    v = torch.cat((v_cache, v), 1)
-                print("k,v", [k.shape, v.shape], InferenceContext.kv_cache[layer_id]) 
-                InferenceContext.kv_cache[layer_id] = [k, v]
-            else:
-                InferenceContext.kv_cache = None
-        except:
-            import traceback
-            print("traceback", traceback.format_exc())
 
-        print(q.shape, k.shape, v.shape, heads, attn_mask.shape)
-                    
-        output = torch_npu.npu_fusion_attention(
-            q, k, v, heads, "BSH",
-            pse=None,
-            padding_mask=None,
-            atten_mask=attn_mask.bool(),
-            scale=norm_factor,
-            pre_tockens=65536,
-            next_tockens=65536,
-            keep_prob=1,
-            inner_precise=0
-        )[0]
+        if not is_promt:
+            k_cache, v_cache = InferenceContext.kv_caches[layer_id]
+            if k_cache is not None:
+                k = torch.cat([k_cache, k], dim=1)
+                v = torch.cat([v_cache, v], dim=1)
+            print("k,v", [k.shape, v.shape]) 
+        InferenceContext.kv_caches[layer_id] = [k, v]
+        pre_seq_len = k.shape[1]
 
-        # [b, s, H] --> [b, s, h, k] --> [b, h, s, k]
-        with torch.no_grad():
-            k = k.view(k.size(0), k.size(1), heads, -1).transpose(1, 2).contiguous()
-            v = v.view(v.size(0), v.size(1), heads, -1).transpose(1, 2).contiguous()
+        q = q.reshape(bsz, seq_len, heads, -1).transpose(1, 2).reshape(bsz*heads, seq_len, -1).contiguous()  # [b * n, s, d]
+        k = k.reshape(bsz, seq_len, (num_kv if num_kv > 0 else heads), -1).transpose(1, 2).reshape(bsz*(num_kv if num_kv > 0 else heads), pre_seq_len, -1).contiguous()  # [b * n, s, d]
+        v = v.reshape(bsz, seq_len, (num_kv if num_kv > 0 else heads), -1).transpose(1, 2).reshape(bsz*(num_kv if num_kv > 0 else heads), pre_seq_len, -1).contiguous()  # [b * n, s, d]
         
-        return output, k, v
+        output, attn_weight_reshaped = NPUInference.replace_npu_fusion_attention(q, k, v, norm_factor, attn_mask, heads, bsz, pre_seq_len)
+
+        # [b * n, s, d] --> [b, n, s, d] --> [b, s, n, d]
+        k_layer = k.reshape(bsz, heads, pre_seq_len, -1).contiguous()
+        v_layer = v.reshape(bsz, heads, pre_seq_len, -1).contiguous()
+
+        return output, k_layer, v_layer
+
+    @staticmethod
+    def replace_npu_fusion_attention(query_layer, key_layer, value_layer, norm_factor, 
+                                     attn_mask, heads, bsz, seq_len):
+        """
+            query_layer: [bsz*heads, seq_len, -1]
+            key_layer,value_layer: [bsz*heads, soft_len, -1]
+        """
+        # [bsz*heads, seq_len, soft_len]
+        attn_score = torch.bmm(query_layer, key_layer.transpose(1, 2))
+        attn_score *= norm_factor * norm_factor
+        print('attn_score', attn_score.shape, attn_mask.shape)
+        if attn_mask is not None:
+            attn_score = attn_score.view(bsz, heads, seq_len, -1).contiguous()
+            attn_score = attn_score + attn_mask
+            attn_score = torch.max(attn_score, torch.tensor(torch.finfo(attn_score.dtype).min, device=attn_score.device))
+            attn_score = attn_score.view(bsz*heads, seq_len, -1).contiguous()
+        print('attn_score', attn_score.shape)
+
+        dtype = attn_score.dtype
+        attn_score = torch.nn.Softmax(dim=-1)(attn_score.float()).to(dtype)
+        attn_weight_reshaped = attn_score.view(bsz, heads, seq_len, -1).contiguous()
+
+        # [b * n, s, soft_len] * [b * n, soft_len, d] --> [b * n, s, d]
+        print(attn_score.shape, value_layer.shape)
+        out = torch.bmm(attn_score, value_layer)
+
+        # [b * n, s, d] --> [b, n, s, d] --> [b, s, n, d] --> [b, s, H]
+        output = out.reshape(bsz, heads, seq_len, -1).permute(0, 2, 1, 3).reshape(bsz, seq_len, -1).contiguous()
+        
+        return output, attn_weight_reshaped
     
     @staticmethod
     def softmax_context_fp32(query_key_value, attn_mask, rotary_dim, 
@@ -264,12 +312,13 @@ class NPUInference():
     def _vector_matmul_(input,
                         weight,
                         transposed_mode):
-        if transposed_mode:
-            output = torch.matmul(input, weight.t())
-        else:
-            output = torch.matmul(input, weight)
-        print("vector matmul", output.shape)
-        return output 
+        return torch.matmul(input, weight.t())
+        # if transposed_mode:
+        #     output = torch.matmul(input, weight.t())
+        # else:
+        #     output = torch.matmul(input, weight)
+        # print("vector matmul", output.shape)
+        # return output 
         
     
     @staticmethod
@@ -322,20 +371,26 @@ class NPUInference():
                    q_int8,
                    activation_type,
                    transposed_mode):
-        print(type(input), type(residual), type(input_bias))
-        if mlp_after_attn:
-            residual_add = torch.nn.functional.layer_norm(input + residual + input_bias, (input.shape[2], ), gamma, beta, epsilon)
-        else:
-            residual_add = torch.nn.functional.layer_norm(input, (input.shape[2], ), gamma, beta,
-                                        epsilon)
+        # print(type(input), type(residual), type(input_bias))
+        # if mlp_after_attn:
+        #     residual_add = torch.nn.functional.layer_norm(input + residual + input_bias, (input.shape[2], ), gamma, beta, epsilon)
+        # else:
+        #     residual_add = torch.nn.functional.layer_norm(input, (input.shape[2], ), gamma, beta,
+        #                                 epsilon)
+        # tmp = torch.matmul(residual_add, weight_interm.t())
+        # from deepspeed.utils.types import ActivationFuncType
+        # if activation_type == ActivationFuncType.GELU:
+        #     tmp = torch.nn.functional.gelu(tmp + bias)
+        # elif activation_type == ActivationFuncType.ReLU:
+        #     tmp = torch.nn.functional.relu(tmp + bias)
+        # output = torch.matmul(tmp, weight_out.t())
+        # return output, residual_add    
+        residual_add = torch.nn.functional.layer_norm(input + residual + input_bias, (input.shape[2],), gamma, beta,
+                                                      epsilon)
         tmp = torch.matmul(residual_add, weight_interm.t())
-        from deepspeed.utils.types import ActivationFuncType
-        if activation_type == ActivationFuncType.GELU:
-            tmp = torch.nn.functional.gelu(tmp + bias)
-        elif activation_type == ActivationFuncType.ReLU:
-            tmp = torch.nn.functional.relu(tmp + bias)
+        tmp = torch.nn.functional.gelu(tmp + bias)
         output = torch.matmul(tmp, weight_out.t())
-        return output, residual_add    
+        return output, residual_add
     
     @staticmethod
     def mlp_gemm_fp32(input,
@@ -449,22 +504,31 @@ class NPUInference():
                            mlp_after_attn,
                            add_bias,
                            preln):
-        if mlp_after_attn:
-            if preln:
-                # residual = (residual + attention + bias + attention_bias) *
-                # mp_scale + hidden_state
-                tmp = (residual + attention_output + attention_bias + final_bias) / mp_size + hidden_state
-            else:
-                # residual += hidden_state + bias
-                tmp = residual + hidden_state + final_bias
+        # if mlp_after_attn:
+        #     if preln:
+        #         # residual = (residual + attention + bias + attention_bias) *
+        #         # mp_scale + hidden_state
+        #         tmp = (residual + attention_output + attention_bias + final_bias) / mp_size + hidden_state
+        #     else:
+        #         # residual += hidden_state + bias
+        #         tmp = residual + hidden_state + final_bias
+        # else:
+        #     if add_bias:
+        #         residual += attention_bias   
+        #     tmp = hidden_state + attention_output + (residual + final_bias) * mp_size
+
+        # input_dtype = hidden_state.dtype
+        # residual = tmp.to(input_dtype)
+        # print("residual output", residual.shape)
+        # return residual
+        if preln:
+            tmp = (residual.float() + attention_output.float() + attention_bias.float() +
+                   final_bias.float()) / mp_size + hidden_state.float()
         else:
-            if add_bias:
-                residual += attention_bias   
-            tmp = hidden_state + attention_output + (residual + final_bias) * mp_size
+            tmp = residual.float() + hidden_state.float() + final_bias.float()
 
         input_dtype = hidden_state.dtype
         residual = tmp.to(input_dtype)
-        print("residual output", residual.shape)
         return residual
 
     @staticmethod
